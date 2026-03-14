a318a9c5ca12c1ae3a401e9fe460f9a692cb4a77
EliasKeller <elias.keller@gmail.com>
2026-03-14 12:46:37 +0100

    feat: disable zoom on whole application by setting viewport

--- src/app/layout.js
+++ src/app/layout.js
@@ -16,6 +16,13 @@ export const metadata = {
   description: "Yatzy App by Elias & Fanouille",
 };
 
+export const viewport = {
+  width: "device-width",
+  initialScale: 1,
+  maximumScale: 1,
+  userScalable: false,
+};
+
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
